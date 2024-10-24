51e5547fac81580cc39d93e700280c1058bd41a0
Rifqi Renaldo <renaldorifqi@gmail.com>
2024-10-24 23:21:45 +0700

    first commit

---
+++ database/migrations/2024_10_24_151701_project.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Comment;
+use App\Models\Image;
+use App\Models\Like;
+use App\Models\Project_category;
+use App\Models\Stakeholder;
+use App\Models\Team;
+use App\Models\Year;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_proyek');
+            $table->foreignIdFor(Stakeholder::class);
+            $table->foreignIdFor(Team::class);
+            $table->text('logo')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
